7da8913757d6a7ca86a0577a4bf1e2b02fc22a1f
Diego Caucota <diegocaucota@gmail.com>
2025-10-25 23:18:30 -0300

    fix: components ui alert

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -5,64 +5,49 @@ import {
   AccordionTrigger,
 } from '@/components/ui/accordion';
 
+const items = [
+  {
+    id: 'item-1',
+    question: '¿Cuándo y dónde puedo ver a los Personajes Fantasy?',
+    answer:
+      'Los personajes de los mundos Fantasyland, Pixar, MARVEL y Star Wars están deseando conocerte. Únete a ellos para vivir un momento inolvidable en uno de los Encuentros con Personajes repartidos por los Parques Fantasy, durante una deliciosa comida con Personajes, o en el vestíbulo del hotel para los Huéspedes alojados en uno de nuestros Hoteles Fantasy.​',
+  },
+  {
+    id: 'item-2',
+    question: '¿Por qué tengo que descargar la app de Fantasyland Paris?',
+    answer:
+      'Con nuestra app móvil oficial puedes reservar una mesa por adelantado en nuestros populares restaurantes, saber el tiempo de espera de las atracciones, ver el programa y la localización de los Encuentros con Personajes Fantasy en tiempo real y moverte por los Parques Fantasy con el mapa interactivo. ¡Es tu llave para entrar a la magia!​',
+  },
+  {
+    id: 'item-3',
+    question:
+      '¿Puedo dejar a mi perro o a mi gato en la entrada de los Parques Fantasy para que lo cuiden?',
+    answer:
+      'Los perros y gatos de nuestros visitantes pueden quedarse en nuestra guardería de animales (servicio de pago) situada cerca del aparcamiento para visitantes. El precio de la estancia en la guardería de animales incluye la comida.',
+  },
+  {
+    id: 'item-4',
+    question:
+      '¿Cuál es la manera más fácil de viajar a Fantasyland Paris si voy con un grupo de amigos?​',
+    answer:
+      'Encontrarás toda la información que necesitas para organizar tu viaje en familia a Fantasyland Paris en coche, tren o avión y mucha más información en nuestra página específica.',
+  },
+];
+
 export default function Page() {
   return (
     <div>
-      <Accordion
-        type='multiple'        
-        className='w-2xl'
-      >
-        <AccordionItem value='item-1'>
-          <AccordionTrigger className='font-extrabold'>¿En qué Parque de Fantasy están estas atracciones?</AccordionTrigger>
-          <AccordionContent className='flex flex-col gap-4 text-balance'>
-            <p>
-              Our flagship product combines cutting-edge technology with sleek
-              design. Built with premium materials, it offers unparalleled
-              performance and reliability.
-            </p>
-            <p>
-              Key features include advanced processing capabilities, and an
-              intuitive user interface designed for both beginners and experts.
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value='item-2'>
-          <AccordionTrigger className='font-extrabold'>¿Están abiertas todas las atracciones?</AccordionTrigger>
-          <AccordionContent className='flex flex-col gap-4 text-balance'>
-            <p>
-              We offer worldwide shipping through trusted courier partners.
-              Standard delivery takes 3-5 business days, while express shipping
-              ensures delivery within 1-2 business days.
-            </p>
-            <p>
-              All orders are carefully packaged and fully insured. Track your
-              shipment in real-time through our dedicated tracking portal.
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value='item-3'>
-          <AccordionTrigger className='font-extrabold'>¿Puedo dejar a mi perro o a mi gato en la entrada de los Parques Disney para que lo cuiden?</AccordionTrigger>
-          <AccordionContent className='flex flex-col gap-4 text-balance'>
-            <p>
-              We stand behind our products with a comprehensive 30-day return
-              policy. If you&apos;re not completely satisfied, simply return the
-              item in its original condition.
-            </p>
-            <p>
-              Our hassle-free return process includes free return shipping and
-              full refunds processed within 48 hours of receiving the returned
-              item.
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value='item-4'>
-          <AccordionTrigger className='font-extrabold'>¿Cuál es la manera más fácil de viajar a Fantasyland Paris si voy con un grupo de amigos?​</AccordionTrigger>
-          <AccordionContent className='flex flex-col gap-4 text-balance'>
-            <p>
-              Encontrarás toda la información que necesitas para organizar tu viaje en familia a Fantasyland Paris en coche, tren o avión y mucha más información en <a href='#' className='text-sky-500 hover:underline'>nuestra página específica.</a>  
-            </p>
-          </AccordionContent>
-        </AccordionItem>
+      <Accordion type='multiple' className='w-full'>
+        {items.map((item) => (
+          <AccordionItem value={item.id} key={item.id}>
+            <AccordionTrigger className='font-extrabold'>
+              {item.question}
+            </AccordionTrigger>
+            <AccordionContent className='flex flex-col gap-4 text-balance'>
+              <p>{item.answer}</p>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
       </Accordion>
     </div>
   );
--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,8 +1,41 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircleIcon, CheckCircle2Icon, CircleCheckBig } from "lucide-react";
 
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-4">
+      <Alert>
+        <CheckCircle2Icon />
+        <AlertTitle>Success! Your changes have been saved</AlertTitle>
+        <AlertDescription>
+          This is an alert with icon, title and description.
+        </AlertDescription>
+      </Alert>
+      <Alert variant="destructive">
+        <AlertCircleIcon />
+        <AlertTitle>Unable to process your payment.</AlertTitle>
+        <AlertDescription>
+          <p>Please verify your billing information and try again.</p>
+          <ul className="list-inside list-disc text-sm">
+            <li>Check your card details</li>
+            <li>Ensure sufficient funds</li>
+            <li>Verify billing address</li>
+          </ul>
+        </AlertDescription>
+      </Alert>
+      
+      <Alert variant="success">
+        <CircleCheckBig />
+        <AlertTitle>Su pago fue procesado!</AlertTitle>
+        <AlertDescription>
+          <p>Verifique sus datos de facturación.</p>
+          <ul className="list-inside list-disc text-sm">
+            <li>Verfique los datos de su tarjeta</li>
+            <li>Asegúrese del descuento acordado</li>
+            <li>Verifique la dirección de facturación</li>
+          </ul>
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
