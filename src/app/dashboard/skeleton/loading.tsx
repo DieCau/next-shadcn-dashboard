db7f4e29d93aa8249afa2fc2f3995a054ab410d0
Diego Caucota <diegocaucota@gmail.com>
2025-10-29 21:47:07 -0300

    fix: components ui skeleton

---
+++ src/app/dashboard/skeleton/loading.tsx
@@ -0,0 +1,29 @@
+import { Card, CardHeader, CardFooter } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function Loading() {
+
+    const data = '123456789'.split('');
+
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-3'>
+            {data.map((item) => (
+                <Card key={item}>
+                    <CardHeader className='flex flex-row items-center'>
+                        <Skeleton
+                            className='rounded-full mr-2 size-10'
+                        />
+                        <div className='flex-col flex-grow'>
+                            <Skeleton className='h-4 w-1/2 mb-2' />
+                            <Skeleton className='h-3 w-full' />
+                        </div>
+                    </CardHeader>
+
+                    <CardFooter className='flex justify-end'>
+                        <Skeleton className='h-8 w-20' />
+                    </CardFooter>
+                </Card>
+            ))}
+        </div>
+    );
+}
--- src/app/dashboard/skeleton/page.tsx
+++ src/app/dashboard/skeleton/page.tsx
@@ -1,20 +1,42 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 
-export default function Page() {
+const getData = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
+  return '123456789'.split('');
+}
+
+export default async function Page() {
+
+  const data = await getData()
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-      <Card>
-        <CardHeader>
-          <Image
-            src='https://github.com/shadcn.png'
-            alt='Shadcn'
-            width={40}
-            height={40}
-            className="rounded-full mr-2 size-10"           
-          />
-        </CardHeader>        
-      </Card>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+      {
+        data.map(item => (
+          <Card key={item}>
+            <CardHeader className="flex flex-row items-center">
+              <Image
+                src='https://github.com/shadcn.png'
+                alt='Shadcn'
+                width={40}
+                height={40}
+                className="rounded-full mr-2 size-10"
+              />
+              <div>
+                <CardTitle>Titulo Card</CardTitle>
+                <CardDescription>Descripcion Card</CardDescription>
+              </div>
+            </CardHeader>
+
+            <CardFooter className="flex justify-end">
+              <Button>Más</Button>
+            </CardFooter>
+          </Card>
+        ))
+      }
     </div>
   );
 }
